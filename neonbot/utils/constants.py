f040362587039e096ee4d57e77476fbbceb7866e
NeonSpectrum <youngskymann@gmail.com>
2022-12-02 04:46:52 +0800

    Cleanup

--- neonbot/utils/constants.py
+++ neonbot/utils/constants.py
@@ -5,37 +5,10 @@ LOG_FORMAT = (
 
 YOUTUBE_TMP_DIR = "./tmp/youtube_dl"
 
-PERMISSIONS = 8
-
-PAGINATION_EMOJI = ["⏮", "◀", "▶", "⏭", "🗑"]
-CHOICES_EMOJI = [
-    "\u0031\u20E3",
-    "\u0032\u20E3",
-    "\u0033\u20E3",
-    "\u0034\u20E3",
-    "\u0035\u20E3",
-    "\u0036\u20E3",
-    "\u0037\u20E3",
-    "\u0038\u20E3",
-    "\u0039\u20E3",
-    "\u0040\u20E3",
-    "🗑",
-]
+PERMISSIONS = 3145728
 
 ICONS = {
-    "pokemon": "https://i.imgur.com/3sQh8aN.png",
     "music": "https://i.imgur.com/SBMH84I.png",
-    "python": "https://i.imgur.com/vzcWouB.png",
-    "github": "https://cdn1.iconfinder.com/data/icons/social-media-vol-1-1/24/_github-512.png",
-    "pip": "https://i.imgur.com/vzcWouB.png",
-    "google": "https://i.imgur.com/G46fm8J.png",
-    "merriam": "https://dictionaryapi.com/images/MWLogo.png",
-    "openweather": "https://media.dragstone.com/content/icon-openweathermap-1.png",
-    "leaguespy": "https://www.leaguespy.net/images/favicon/favicon-32x32.png",
-    "azlyrics": "https://images.azlyrics.com/az_logo_tr.png",
-    "semaphone": "https://semaphore.co/images/pages/index/semaphore-icon.png",
-    "myanimelist": "https://i.imgur.com/XMQsLF5.png",
-    "googletranslate": "https://upload.wikimedia.org/wikipedia/commons/d/db/Google_Translate_Icon.png"
 }
 
 FFMPEG_OPTIONS = (
@@ -44,9 +17,6 @@ FFMPEG_OPTIONS = (
 
 YOUTUBE_REGEX = r"^(http(s)?:\/\/)?(((w){3}|music).)?youtu(be|.be)?(\.com)?\/.+"
 SPOTIFY_REGEX = r"^(spotify:|https:\/\/[a-z]+\.spotify\.com\/)"
-
-IGNORED_DELETEONCMD = ["eval", "prune"]
-EXCLUDED_TYPING = ["eval", "prune", "skip", "chatbot"]
 
 LOGO = """\
  __    _  _______  _______  __    _  _______  _______  _______
